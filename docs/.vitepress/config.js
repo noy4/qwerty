c8d43fbbdb898b310006eda35c5621ca6baa1a96
noy4 <naoyan418@gmail.com>
2021-04-23 13:17:54 +0900

    test travis

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,18 +1,54 @@
 module.exports = {
   title: 'Qwerty',
   description: "Hi, I'm Qwerty.",
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Qwerty',
+      description: "Hi, I'm Qwerty.",
+    },
+    '/ja/': {
+      lang: 'ja-JP',
+      title: 'Qwerty',
+      description: 'こんにちは、クワーティーです。',
+    },
+  },
   themeConfig: {
-    sidebar: [
-      { text: 'Introduction', link: '/introduction' },
-      {
-        text: 'Works',
-        children: [
-          { text: 'Web Programming', link: '/web' },
-          { text: 'Video Editing', link: '/video' },
-          { text: 'Music', link: '/music' },
+    locales: {
+      '/': {
+        label: 'English',
+        selectText: 'Language',
+        sidebar: [
+          { text: 'Introduction', link: '/introduction' },
+          {
+            text: 'Works',
+            children: [
+              { text: 'Web Programming', link: '/web' },
+              { text: 'Video Editing', link: '/video' },
+              { text: 'Music', link: '/music' },
+            ],
+          },
+          { text: 'Links', link: '/links' },
         ],
+        nav: [],
       },
-      { text: 'Links', link: '/links' },
-    ],
+      '/ja/': {
+        label: '日本語',
+        selectText: '言語',
+        sidebar: [
+          { text: '自己紹介', link: '/ja/introduction' },
+          {
+            text: '作品',
+            children: [
+              { text: 'Webプログラミング', link: '/ja/web' },
+              { text: '動画編集', link: '/ja/video' },
+              { text: '音楽', link: '/ja/music' },
+            ],
+          },
+          { text: 'Links', link: '/ja/links' },
+        ],
+        nav: [],
+      },
+    },
   },
 }
---
+++ docs/ja/index.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: ./kuwa_graduate_icon.png
+actionText: Get Started →
+actionLink: introduction
+features:
+- title: いいやつ
+  details: Qwerty is a nice guy. When you're in trouble, ask him for help. He's gonna solve every problem you're suffering from.
+- title: Kindhearted
+  details: Qwerty is kindhearted. When he moves his hands, or his legs, or even his body, he generates "kindness".
+- title: Fascinating
+  details: Qwerty is fascinating. Don't look into his eyes, or you'll fall in love with him.
+footer: Copyright © 2021 Naoya Kuwamura
+---
